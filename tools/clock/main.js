eefc8df676915bcc5dd2704609986cc94ca4a2bf
Ollie Relph <ollie@relph.me>
2013-06-27 11:39:03 +0100

    Add required API methods

--- tools/clock/main.js
+++ tools/clock/main.js
@@ -134,6 +134,38 @@ define(['cocos2d'], function(cocos2d) {
 
     });
 
+    ToolLayer.create = function () {
+        var sg = new ToolLayer();
+        if (sg && sg.init(cc.c4b(255, 255, 255, 255))) {
+            return sg;
+        }
+        return null;
+    };
+
+    ToolLayer.scene = function () {
+        var scene = cc.Scene.create();
+        var layer = ToolLayer.create();
+        scene.addChild(layer);
+
+        scene.layer=layer;
+
+        // scene.setMouseEnabled(true);
+        // scene.onMouseDown=function(event){cc.log("mouse down");};
+
+        scene.ql=new QLayer();
+        scene.ql.init();
+        layer.addChild(scene.ql, 99);
+
+        scene.update = function(dt) {
+            this.layer.update(dt);
+            this.ql.update(dt);
+        };
+        scene.scheduleUpdate();
+
+
+        return scene;
+    };
+
     var Clock = cc.Sprite.extend({
         linkedClock:null,
         time:null,
