b624d0c412fbc280edfac2668ef9261a5cafeecc
Ollie Relph <ollie@relph.me>
2013-07-04 14:12:52 +0100

    Include all file types (except js) as well as directories

--- compile_resources.sh
+++ compile_resources.sh
@@ -7,23 +7,34 @@ resources=()
 
 #images first
 echo "Compiling Resources"
-echo "  Collecting Images"
-echo "// Images" >> $targetFile
+echo "  Collecting Files"
 i=0
-for f in `find ./shared-resources -iname "*.png" -not \( -iname "*@2x.png" \)`
+for f in `find ./shared-resources -not \( -iname "*@2x.png" -o -iname "*.js" \)`
 do
+	# remove the starting .
 	strippedPath=`echo "$f" | sed "s/\.\//\//g"`
-	varName=`echo "$strippedPath" | sed "s/\/shared\-resources\/images\///g" | sed "s/\//\_/g" | sed "s/\.png//" | sed "s/\-/\_/g" | awk '{print tolower($0)}'`
-	echo "var $varName = \"$strippedPath\";" >> $targetFile
-	resources[$i]="$varName"
-	i=$((i+1))
+	# clean up the filename
+	fileNameClean=`echo "$strippedPath" | sed "s/\/shared\-resources\///g" | sed "s/\/images\///g" | sed "s/\/fonts\///g" | sed "s/\//\_/g" | sed "s/\-/\_/g" | awk '{print tolower($0)}'`
+	#remove the extension
+	varName=${fileNameClean%.*}
+
+	# if this is not a directory
+	if [[ $fileNameClean == *.* ]]
+	then
+		echo "var $varName = \"$strippedPath\";" >> $targetFile
+		resources[$i]="$varName"
+		i=$((i+1))
+	else
+		# append a trailing slash
+		echo "var $varName = \"$strippedPath/\";" >> $targetFile
+	fi
 done
 
 echo "  Writing Resources"
 echo "// Cocos Preloaded Resources" >> $targetFile
 echo "var g_resources = [" >> $targetFile
 
-
+# get the last index of the array
 last=$(( ${#resources[*]} - 1 ))
 for r in "${!resources[@]}"
 do
