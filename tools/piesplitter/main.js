53acf389bcd381cf66f6208ab1320af835d9de94
Alex Jeffreys <aj@belugalearning.com>
2013-07-22 10:54:13 +0100

    Start of pie splitter

---
+++ tools/piesplitter/main.js
@@ -0,0 +1,49 @@
+require.config({
+	paths: {
+		'toollayer': '../../tools/common/toollayer'
+	}
+});
+
+define(['exports', 'cocos2d', 'toollayer', 'qlayer'], function(exports, cocos2d, ToolLayer, QLayer) {
+	'use strict';
+
+	window.bl.toolTag = 'piesplitter';
+
+	var Tool = ToolLayer.extend({
+
+		init:function() {
+			this._super();
+
+			this.setTouchEnabled(true);
+
+            this.size = cc.Director.getInstance().getWinSize();
+            var size = this.size;
+
+            var clc = cc.Layer.create();
+            var background = new cc.Sprite();
+            background.initWithFile(window.bl.getResource('images_deep_water_background'));
+            background.setPosition(size.width/2, size.height/2);
+            clc.addChild(background);
+            this.addChild(clc,0);
+
+            this.dividend = 3;
+            this.divisor = 4;
+
+            this.fullBubbles = [];
+
+            for (var i = 0; i < this.dividend; i++) {
+            	var bubble = new cc.Sprite();
+            	bubble.initWithFile(window.bl.getResource('expanded_slice1'));
+            	bubble.setPosition(300 + 200 * i, 500);
+            	this.addChild(bubble);
+            };
+
+            return this;
+
+		},
+
+	});
+
+	exports.ToolLayer = Tool;
+
+});
