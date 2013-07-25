3748554b43a03f139cb3365b14e1920113c3c72c
Alex Jeffreys <aj@belugalearning.com>
2013-07-25 12:34:19 +0100

    Correctly spaces mini pies

--- tools/piesplitter/pie-hole.js
+++ tools/piesplitter/pie-hole.js
@@ -26,14 +26,36 @@ define(['pie', 'piepiece'], function(Pie, PiePiece) {
 			miniPie.setPosition(0, -(this.miniPies.length * 40 + 75));
 			this.miniPies.push(miniPie);
 			this.addChild(miniPie);
+			this.positionMiniPies();
 		},
 
 		removeSelectedPiePiece:function() {
 			this._super();
 			if (this.piePieces.length === 0 && this.miniPies.length > 0) {
-				this.miniPies[this.miniPies.length - 1].removeFromParent();
-				this.miniPies.splice(this.miniPies.length - 1, 1);
+				this.removeMiniPie();
 				this.fillPie();
+			};
+		},
+
+		removeMiniPie:function() {
+			this.miniPies[this.miniPies.length - 1].removeFromParent();
+			this.miniPies.splice(this.miniPies.length - 1, 1);
+			this.positionMiniPies();
+		},
+
+		positionMiniPies:function() {
+			var spacingAngle = 2*Math.PI/9;
+			var numberOfPies = this.miniPies.length;
+			var baseAngle = Math.PI + (numberOfPies-1)/2 * -spacingAngle;
+			var pieRadius = this.pieCover.getBoundingBox().size.width/2;
+			var miniPieRadius = this.miniPies.length > 0 ? this.miniPies[0].getBoundingBox().size.width/2 : 0;
+			var radius = pieRadius + miniPieRadius + 3;
+			for (var i = 0; i < numberOfPies; i++) {
+				var angle = baseAngle + i * spacingAngle;
+				var xPosition = radius * Math.sin(angle);
+				var yPosition = radius * Math.cos(angle);
+				var miniPie = this.miniPies[i];
+				miniPie.setPosition(xPosition, yPosition);
 			};
 		},
 
